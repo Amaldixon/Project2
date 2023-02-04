0557c87be7865307ffb1dd96317b7dd3b36b738d
Amaldixon <amaldixon22@gmail.com>
2023-02-04 12:34:11 +0000

    index.html

---
+++ app.js
@@ -0,0 +1,9 @@
+function flip(guess) {
+    let sideLandedOn = Math.round(Math.random()) == 0 ? 'heads' : 'tails'; // "flip" coin
+    document.getElementById("result").innerHTML = sideLandedOn; // set innerHTML of results element to side that coin landed on
+    if (guess == sideLandedOn) {
+      alert(`The coin landed on ${sideLandedOn} and you guessed ${guess}... you win!`); // alert user won
+    } else {
+      alert(`The coin landed on ${sideLandedOn} and you guessed ${guess}... you lose!`); // alert user lost
+    }
+  }

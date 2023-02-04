a6796557bf6806eddf79788962268ad1a59d3c59
Amaldixon <113308438+Amaldixon@users.noreply.github.com>
2023-02-04 12:37:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function flip(guess) {
     let sideLandedOn = Math.round(Math.random()) == 0 ? 'heads' : 'tails'; // "flip" coin
-    document.getElementById("result").innerHTML = sideLandedOn; // set innerHTML of results element to side that coin landed on
+    document.getElementById("result").innerHTML = sideLandedOn; // results element to side that coin landed on
     if (guess == sideLandedOn) {
       alert(`The coin landed on ${sideLandedOn} and you guessed ${guess}... you win!`); // alert user won
     } else {

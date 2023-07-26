4227f9f40219b6d1e9bce23dca5e28e4ca97cb26
Amaldixon <113308438+Amaldixon@users.noreply.github.com>
2023-07-26 10:19:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,62 @@
-function flip(guess) {
-    let sideLandedOn = Math.round(Math.random()) == 0 ? 'heads' : 'tails'; // "flip" coin
-    document.getElementById("result").innerHTML = sideLandedOn; // results element to side that coin landed on
-    if (guess == sideLandedOn) {
-      alert(`The coin landed on ${sideLandedOn} and you guessed ${guess}... you win!`); // alert user won
-    } else {
-      alert(`The coin landed on ${sideLandedOn} and you guessed ${guess}... you lose!`); // alert user lost
-    }
+let playerScore = 0;
+let computerScore = 0;
+let roundsPlayed = 0;
+
+function playRound(playerSelection) {
+  if (roundsPlayed >= 3) {
+    resetGame();
   }
+
+  const choices = ["rock", "paper", "scissors"];
+  const computerSelection = choices[Math.floor(Math.random() * choices.length)];
+
+  if (playerSelection === computerSelection) {
+    showResult("It's a tie!");
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    showResult("You win this round!");
+    playerScore++;
+  } else {
+    showResult("You lose this round!");
+    computerScore++;
+  }
+
+  roundsPlayed++;
+  updateScore();
+
+  if (playerScore === 2 || computerScore === 2) {
+    showMatchResult();
+  }
+}
+
+function showResult(result) {
+  const resultElement = document.getElementById("result");
+  resultElement.textContent = result;
+}
+
+function updateScore() {
+  const scoreElement = document.getElementById("score");
+  scoreElement.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+}
+
+function showMatchResult() {
+  const matchResultElement = document.getElementById("match-result");
+  if (playerScore > computerScore) {
+    matchResultElement.textContent = "Congratulations! You win the match!";
+  } else {
+    matchResultElement.textContent = "You lose the match. Better luck next time!";
+  }
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  roundsPlayed = 0;
+  showResult("");
+  updateScore();
+  const matchResultElement = document.getElementById("match-result");
+  matchResultElement.textContent = "";
+}
